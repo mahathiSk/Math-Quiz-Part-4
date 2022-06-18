7f85ab7edfda29010d78da06801abe8badef4b4a
mahathiSk <92446223+mahathiSk@users.noreply.github.com>
2022-06-18 20:00:30 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -4,11 +4,20 @@ player2 =0;
 player1_name = localStorage.getItem("player1_name");
 player2_name = localStorage.getItem("player2_name");
 
-question_turn =player1;
-answer_turn =player2;
+question_turn ="player1";
+answer_turn ="player2";
 
 player1_score = 0;
 player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
 
 function check() {
     get_answer =document.getElementById("input_check_box").value;
@@ -24,32 +33,28 @@ function check() {
     }
 
     if(question_turn =="player1"){
-        question_turn =player2;
+        question_turn ="player2";
      document.getElementById("player_question").innerHTML ="Question Turn - "+player2_name;
+     console.log(player2_name)
     }
     else{
-        question_turn =player1;
+        question_turn ="player1";
      document.getElementById("player_question").innerHTML ="Question Turn - "+player1_name;
+    
     }
 
     if(answer_turn=="player1"){
-        answer_turn =player2;
+        answer_turn ="player2";
      document.getElementById("player_answer").innerHTML ="Answer Turn - "+player2_name;
     }
     else{
-        answer_turn =player1;
+        answer_turn ="player1";
      document.getElementById("player_answer").innerHTML ="Answer Turn - "+player1_name;
     }
+    document.getElementById("output").innerHTML ="";
 }
 
-document.getElementById("player1_name").innerHTML = player1_name + " : ";
-document.getElementById("player2_name").innerHTML = player2_name + " : ";
 
-document.getElementById("player1_score").innerHTML = player1_score;
-document.getElementById("player2_score").innerHTML = player2_score;
-
-document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
-document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
 
 function send() {
     number1 = document.getElementById("number_1").value;
